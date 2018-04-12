dbe12f37591b2e77940589061c8c3a9c2e397ed1
avaKados <36055041+avaKados@users.noreply.github.com>
2018-04-12 02:46:44 -0400

    Add files via upload

---
+++ censorBot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.io');
+var auth = require('./auth.json');
+
+//initialize bot
+var bot = new Discord.client({
+		token: "", autorun: true
+});
+
+bot.on('ready', function(){
+	console.log('Logged in as %s - %s\n', bot.username, bot.id);
+});
+
+bot.on('message', function(user, userID, channelId, message, event){
+		if(message == 'ping'){
+			bot.sendMessage({
+				to: channelID,
+				message: "pong"
+			});
+		}
+});
